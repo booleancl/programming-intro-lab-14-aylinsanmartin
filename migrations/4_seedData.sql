04a2edd92262a6c5a429da3d1c0c4f2e8f4a1230
aylin <a.sanlazo@gmail.com>
2022-10-14 10:54:48 -0300

    cardinalidad de relaciones

--- migrations/4_seedData.sql
+++ migrations/4_seedData.sql
@@ -1,1 +1,6 @@
+\c blog
+
+\COPY users(email) FROM 'C:/Users/user/intro_dev/programming-intro-lab-14-aylinsanmartin/data/users.csv' csv HEADER;
+\COPY posts FROM 'C:/Users/user/intro_dev/programming-intro-lab-14-aylinsanmartin/data/posts.csv' csv HEADER;
+\COPY comments FROM 'C:/Users/user/intro_dev/programming-intro-lab-14-aylinsanmartin/data/comentarios.csv'csv HEADER;
 
